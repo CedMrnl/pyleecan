b7695cd7bef3a624eccda6c3657d92328348529d
CedMrnl <cedric.marinel@eomys.com>
2020-09-30 18:13:46 +0200

    [WP] Try another femm installation

--- Tests/installation_femm.py
+++ Tests/installation_femm.py
@@ -3,48 +3,35 @@ import pywinauto
 import time
 
 import urllib.request
-import os
-import subprocess
-from shutil import copyfile
 
-# print("Downloading FEMM installer")
-# url = "http://www.femm.info/wiki/Files/files.xml?action=download&file=femm42bin_x64_21Apr2019.exe"
-# urllib.request.urlretrieve(url, "femm42bin_x64_21Apr2019.exe")
-# print("Done")
+print("Downloading FEMM installer")
+url = "http://www.femm.info/wiki/Files/files.xml?action=download&file=femm42bin_x64_21Apr2019.exe"
+urllib.request.urlretrieve(url, "femm42bin_x64_21Apr2019.exe")
+print("Done")
 
-# print("Installing FEMM...")
-os.system("choco new femm")
+print("Installing FEMM...")
 
-copyfile("Tests/femm.nuspec", "Tests/femm/femm.nuspec")
-copyfile("chocolateyinstall.ps1", "Tests/femm/tools/chocolateyinstall.ps1")
-os.system("choco pack Tests/femm/femm.nuspec")
-process = subprocess.Popen(["choco", "install", "Tests/femm.1.0.0.nupkg", "-y"])
 # app = Application(backend="uia").start("femm42bin_x64_21Apr2019.exe")
-# app = Application().start("femm42bin_x64_21Apr2019.exe")
-time.sleep(25)
-try:
-    # Connect to FEMM Setup
-    app = Application()
-    app.connect(best_match="Setup")
-    window = app.top_window()
+app = Application().start("femm42bin_x64_21Apr2019.exe")
+time.sleep(5)
 
-    print(window)
-    # First page: License Agreement
-    window.RadioButton0.click()  # I accept the agreements
-    window.Button.click()  # Next
+# Connect to FEMM Setup
+app.connect(best_match="Setup")
+window = app.top_window()
 
-    # Second page: Select Destination Location
-    window.Button3.click()
+# First page: License Agreement
+window.RadioButton0.click()  # I accept the agreements
+window.Button.click()  # Next
 
-    # Third page: Select Start Menu Folder
-    window.Button3.click()
+# Second page: Select Destination Location
+window.Button3.click()
 
-    # Fourth page: Ready to Install
-    window.Button2.click()
-    time.sleep(30)
+# Third page: Select Start Menu Folder
+window.Button3.click()
 
-    # Close the Setup programm
-    window.Button0.click()
+# Fourth page: Ready to Install
+window.Button2.click()
 
-finally:
-    process.kill()
+# Close the Setup programm
+window.Button0.click()
+
