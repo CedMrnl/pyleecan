80f92e3febb9fb5353f0ad10a230bede166db4ea
CedMrnl <cedric.marinel@eomys.com>
2020-09-30 18:33:46 +0200

    test

--- Tests/installation_femm.py
+++ Tests/installation_femm.py
@@ -17,6 +17,17 @@ time.sleep(5)
 
 # Connect to FEMM Setup
 app.connect(best_match="Setup")
+import psutil
+# Iterate over all running process
+for proc in psutil.process_iter():
+    try:
+        # Get process name & pid from process object.
+        processName = proc.name()
+        processID = proc.pid
+        print(processName , ' ::: ', processID)
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        pass
+
 window = app.top_window()
 
 # First page: License Agreement
